8396c150ddaf72fcbfa0c3896d07caa9cf925776
Your Name <you@example.com>
2023-11-28 21:24:09 +1100

    [CHANGES] progress bar

--- endpage.js
+++ endpage.js
@@ -3,6 +3,8 @@ saveScoreButton = document.getElementById("saveScoreButton");
 mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore = document.getElementById("finalScore");
 finalScore.innerText = mostRecentScore;
+highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
 // provides what key on the keyboard is being pressed
 username.addEventListener("keyup", () => {
     console.log(username.value);
@@ -14,4 +16,18 @@ username.addEventListener("keyup", () => {
 saveHighScore = holder => {
     console.log("clicked saved");
     holder.preventDefault();
+    score = {
+        score: MostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    // to arrange highscores accurately
+    highScores.sort( (a,b) => {
+        return b.score - a.score;
+    })
+    // only top 5 highest scores are kept
+    highScores.splice(5);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    console.log(highScores);
+    window.location.assign("/homepage.html")
 }
