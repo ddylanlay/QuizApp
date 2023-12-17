88911a884e2d06477eb8f3884211a17d1922d7e0
Your Name <you@example.com>
2023-12-17 17:32:51 +1100

    [ADD] config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-2_1OCmsgO5M',
+        userPoolClientId: '7b7jmj1nn6gqpr1nf59avs1nlm',
+        region: 'ap-southeast-2'
+    },
+    api: {
+        invokeUrl: ''
+    }
+};
