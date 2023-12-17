8f0a3f4a23210eb825ee39b98be32460b1ef42aa
ddylanlay <109592570+ddylanlay@users.noreply.github.com>
2023-12-17 17:50:41 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_1OCmsgO5M',
-        userPoolClientId: '7b7jmj1nn6gqpr1nf59avs1nlm',
+        userPoolId:
+        userPoolClientId:
         region: 'ap-southeast-2'
     },
     api: {
